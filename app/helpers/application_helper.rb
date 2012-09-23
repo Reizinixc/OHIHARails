009e47baa44a0091e3788e19c66ff2096beeaedf
Reizinixc <Reizinixc@reireiz.tk>
2012-09-23 13:43:12 +0700

    Added avatar system with Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def gravatar_for(user, width, height)
+    gravatar_id  = Digest::MD5::hexdigest(user.alt_email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url,
+              :alt    => "#{user.name} #{user.lastname}'s photo",
+              :id     => "gravatar",
+              :width  => width,
+              :height => height
+  end
+
 end
