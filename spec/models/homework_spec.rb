b17e72ff4d6c531c8597957fd66b54ec573ce38e
Wongsapat Tangwanidgoon <ong_dkrab@gmail.com>
2012-09-25 13:41:05 +0700

    Add test homework

--- spec/models/homework_spec.rb
+++ spec/models/homework_spec.rb
@@ -1,5 +1,56 @@
 require 'spec_helper'
 
 describe Homework do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Data Schema" do
+    it { should have_db_column(:section_id).of_type(:integer) }
+    it { should have_db_column(:title).of_type(:string) }
+    it { should have_db_column(:description).of_type(:text)}
+    it { should have_db_column(:start_time).of_type(:datetime) }
+    it { should have_db_column(:due_time).of_type(:datetime) }
+    it { should have_db_column(:type).of_type(:string) }
+  end
+
+
+  context "Association" do
+    it { should belong_to :section }
+  end
+
+  context "Validation" do
+    it { should validate_uniqueness_of :section_id }
+    it { should validate_format_of(:section_id).with /[0-9]{3}/ }
+    it { should validate_presence_of :section_id }
+  end
+
+  context "Add a section" do
+    before(:each) do
+      @section = Section.new(:section_id => "1",
+                           :title => "Homework1",
+                           :description => "This homework for test it.")
+    end
+
+    it "must be valid by a default attribute" do
+      @section.should be_valid
+    end
+
+    describe "section" do
+      it "should reject < 3 digits course code" do
+        @section.section_id = "1"
+        @section.should_not_valid
+        @section.should have(1).error_on(:section_id)
+      end
+
+      it "should reject > 3 digit course code" do
+        @section.section_id = "5103"
+        @section.should_not_valid
+        @section.should have(1).error_on(:section_id)
+      end
+
+      it "should reject not integer" do
+        @section.section_id = "a1234"
+        @section.should_not_valid
+        @section.should have(1).error_on(:section_id)
+      end
+    end
+  end
 end
+
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,52 @@
 require 'spec_helper'
 
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Data Schema" do
+    it { should have_db_column(:question).of_type(:text) }
+    it {should have_db_column(:question_homework_id).of_type(:type)}
+
+  end
+
+
+  context "Association" do
+    it { should belong_to :section }
+  end
+
+  context "Validation" do
+    it { should validate_uniqueness_of :section_id }
+    it { should validate_format_of(:section_id).with /[0-9]{3}/ }
+    it { should validate_presence_of :section_id }
+  end
+
+  context "Add a section" do
+    before(:each) do
+      @section = Section.new(:section_id => "1",
+                             :title => "Homework1",
+                             :description => "This homework for test it.")
+    end
+
+    it "must be valid by a default attribute" do
+      @section.should be_valid
+    end
+
+    describe "section" do
+      it "should reject < 3 digits course code" do
+        @section.section_id = "1"
+        @section.should_not_valid
+        @section.should have(1).error_on(:section_id)
+      end
+
+      it "should reject > 3 digit course code" do
+        @section.section_id = "5103"
+        @section.should_not_valid
+        @section.should have(1).error_on(:section_id)
+      end
+
+      it "should reject not integer" do
+        @section.section_id = "a1234"
+        @section.should_not_valid
+        @section.should have(1).error_on(:section_id)
+      end
+    end
+  end
 end
