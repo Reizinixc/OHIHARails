92a5b862c7c1634ccec22508ffeb206771d737b6
Reizinixc <Reizinixc@reireiz.tk>
2012-09-25 11:43:29 +0700

    Add index controller working

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -3,19 +3,37 @@ class HomeworksController < ApplicationController
   before_filter :require_teacher, :except => [:index, :handin]
 
   def index
-    if teacher? or admin?
-      # show homework management data
-    else
-      # show your section's homework
+    sections = []
+    table = teacher? or admin? ? Teach : Takes
+
+    # show homework management data
+    table.find_by_user(current_user).each do |r|
+      sections << r.section_id
+    end
+    begin
+      @homeworks = Homework.find sections
+    rescue RecordNotFound
+      # Redirect to 501 pages
+
     end
   end
 
   def create
+    # Go to homepage create
   end
 
+
   def handin
+    @homework = Homework.find(params[:id])
+    if is_self_homework?
+      # Redirect to Single homework sending page
+    else
+      # Redirect to QUestion homework page
+    end
   end
 
   def download
+    # Do something for get all the student homework
   end
+
 end
--- app/helpers/homeworks_helper.rb
+++ app/helpers/homeworks_helper.rb
@@ -1,2 +1,11 @@
 module HomeworksHelper
+
+  def is_self_homework?(homework)
+    homework.type.instance_of?(SelfHomework)
+  end
+
+  def is_question_homework?(homework)
+    homework.type.instance_of?(QuestionHomework)
+  end
+
 end
